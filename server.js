279f8adba8a38a80f4357d3c181135cdb3eace80
Chris E <VikingsAttack@outlook.com>
2018-11-28 08:23:11 -0800

    Add project page

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render(`maintenance.hbs`);
-})
+// app.use((req, res, next) => {
+//   res.render(`maintenance.hbs`);
+// })
 
 app.use(express.static(__dirname + '/public'));
 
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
     currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
